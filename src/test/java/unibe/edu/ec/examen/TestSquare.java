67ebeaf8c26b7be0d2ea0c8ef26ff4cb0232e9b7
Usuario <Usuario@DESKTOP-1V9AS6J>
2020-06-01 11:28:29 -0500

    Test calculateArea

---
+++ src/test/java/unibe/edu/ec/examen/TestSquare.java
@@ -0,0 +1,21 @@
+package unibe.edu.ec.examen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSquare {
+	public Square square;
+	@Before
+	public void before() {
+		square= new Square();
+		double side=4.5;
+		square.setSide(side);
+	}
+	@Test
+	public void testcalculateArea () {
+		assertEquals(20.25, square.calculateArea(),0.10);
+	}
+	
+}
